17c67e1b8c87be35c462f3ba908710397cde8429
GewoonJelmer <jelmer339@gmail.com>
2021-03-16 16:19:31 +0100

    spawning button

--- Assets/Scripts/controllers/TouchScreenModule.cs
+++ Assets/Scripts/controllers/TouchScreenModule.cs
@@ -2,22 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class TouchScreenModule : MonoBehaviour
 {
 	Dictionary<string, Button> arrows;
+	List<Vector3> locations;
+	GameObject touchscreeners;
 
 	private void Start()
 	{
+		touchscreeners = GameObject.Find("touchscreen");
 		getArrows();
+		getLocations();
 
-		makeButtonSet("green", new Vector3()); 
+		makeButtonSet("green", new Vector3(0,0,0)); 
 	}
 
 
 	void makeButtonSet(string color, Vector3 location)
 	{
+		makeButton(color,location, Quaternion.identity);
+	}
 
+
+	void makeButton(string color, Vector3 location, Quaternion rotation)
+	{
+		var selected = arrows[color];
+		Button button = Instantiate(selected, location, rotation, touchscreeners.transform);
+		button.gameObject.SetActive(true);
 	}
 
 	void getArrows()
@@ -56,5 +69,14 @@ public class TouchScreenModule : MonoBehaviour
 			thisbtn.gameObject.SetActive(false);
 		}
 
+	}
+
+	void getLocations()
+	{
+		locations = new List<Vector3>();
+		locations.Add(Vector3.up);
+		locations.Add(Vector3.down);
+		locations.Add(Vector3.left);
+		locations.Add(Vector3.right);
 	}
 }
